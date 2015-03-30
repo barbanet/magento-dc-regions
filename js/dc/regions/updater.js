c1f85674d629e1d8b86627604bfb9e3c197ae9d6
Damián Culotta <damianculotta@gmail.com>
2015-03-30 16:04:23 -0300

    RegionUpdater overload to allow the sort order by region name.

---
+++ js/dc/regions/updater.js
@@ -0,0 +1,117 @@
+/**
+ * Dc_Regions
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Dc
+ * @package    Dc_Regions
+ * @copyright  Copyright (c) 2015 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Solution copied from http://magento.stackexchange.com/questions/17981/alphabetically-sort-the-state-province-drop-down
+ */
+
+RegionUpdater.addMethods({
+    update: function()
+    {
+        if (this.regions[this.countryEl.value]) {
+            var i, option, region, def;
+
+            def = this.regionSelectEl.getAttribute('defaultValue');
+            if (this.regionTextEl) {
+                if (!def) {
+                    def = this.regionTextEl.value.toLowerCase();
+                }
+                this.regionTextEl.value = '';
+            }
+
+            this.regionSelectEl.options.length = 1;
+            for (regionId in this.regions[this.countryEl.value]) {
+                region = this.regions[this.countryEl.value][regionId];
+
+                option = document.createElement('OPTION');
+                option.value = regionId;
+                option.text = region.name.stripTags();
+                option.title = region.name;
+
+                if (this.regionSelectEl.options.add) {
+                    this.regionSelectEl.options.add(option);
+                } else {
+                    this.regionSelectEl.appendChild(option);
+                }
+
+                if (regionId==def || (region.name && region.name.toLowerCase()==def) ||
+                    (region.name && region.code.toLowerCase()==def)
+                    ) {
+                    this.regionSelectEl.value = regionId;
+                }
+            }
+            this.sortSelect();
+            if (this.disableAction=='hide') {
+                if (this.regionTextEl) {
+                    this.regionTextEl.style.display = 'none';
+                }
+
+                this.regionSelectEl.style.display = '';
+            } else if (this.disableAction=='disable') {
+                if (this.regionTextEl) {
+                    this.regionTextEl.disabled = true;
+                }
+                this.regionSelectEl.disabled = false;
+            }
+            this.setMarkDisplay(this.regionSelectEl, true);
+        } else {
+            this.regionSelectEl.options.length = 1;
+            if (this.disableAction=='hide') {
+                if (this.regionTextEl) {
+                    this.regionTextEl.style.display = '';
+                }
+                this.regionSelectEl.style.display = 'none';
+                Validation.reset(this.regionSelectEl);
+            } else if (this.disableAction=='disable') {
+                if (this.regionTextEl) {
+                    this.regionTextEl.disabled = false;
+                }
+                this.regionSelectEl.disabled = true;
+            } else if (this.disableAction=='nullify') {
+                this.regionSelectEl.options.length = 1;
+                this.regionSelectEl.value = '';
+                this.regionSelectEl.selectedIndex = 0;
+                this.lastCountryId = '';
+            }
+            this.setMarkDisplay(this.regionSelectEl, false);
+        }
+
+        this._checkRegionRequired();
+        // Make Zip and its label required/optional
+        var zipUpdater = new ZipUpdater(this.countryEl.value, this.zipEl);
+        zipUpdater.update();
+    },
+    sortSelect : function (){
+        elem = this.regionSelectEl;
+        var tmpAry = new Array();
+        var currentVal = $(elem).value;
+        for (var i=0;i<$(elem).options.length;i++) {
+            if (i == 0) continue;
+            tmpAry[i-1] = new Array();
+            tmpAry[i-1][0] = $(elem).options[i].text;
+            tmpAry[i-1][1] = $(elem).options[i].value;
+        }
+        tmpAry.sort();
+        while ($(elem).options.length > 0) {
+            $(elem).options[0] = null;
+        }
+        for (var i=1;i<=tmpAry.length;i++) {
+            var op = new Option(tmpAry[i-1][0], tmpAry[i-1][1]);
+            $(elem).options[i] = op;
+        }
+        $(elem).value = currentVal;
+        return;
+    }
+});
