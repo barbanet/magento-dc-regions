502661cbe917413d2ca1130588eed1faec6b9b6b
Damián Culotta <damianculotta@gmail.com>
2015-06-15 22:38:08 -0300

    Fixed admin route.

--- app/code/community/Dc/Regions/controllers/Adminhtml/RegionsController.php
+++ app/code/community/Dc/Regions/controllers/Adminhtml/RegionsController.php
@@ -14,7 +14,7 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Dc_Regions_Adminhtml_ManagerController extends Mage_Adminhtml_Controller_Action
+class Dc_Regions_Adminhtml_RegionsController extends Mage_Adminhtml_Controller_Action
 {
 
     protected function _initAction()
