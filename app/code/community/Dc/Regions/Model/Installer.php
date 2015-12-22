9c33e9d3e1a030e9b8d304ac23f01f8f50d6c35c
Damián Culotta <damianculotta@gmail.com>
2015-12-22 10:04:03 -0300

    Removed alredyInstalled validation.

--- app/code/community/Dc/Regions/Model/Installer.php
+++ app/code/community/Dc/Regions/Model/Installer.php
@@ -16,11 +16,11 @@
 
 class Dc_Regions_Model_Installer extends Varien_Object
 {
-    protected $_regions = null;
+
     /**
      * @var array
      */
-    private $valid_countries = array('AR', 'BR','UY');
+    private $valid_countries = array('AR', 'BR', 'UY');
 
     /**
      * @return array
@@ -29,9 +29,8 @@ class Dc_Regions_Model_Installer extends Varien_Object
     {
         $countries = Mage::getModel('directory/country')->getCollection();
         $options = array();
-
         foreach ($countries as $value) {
-            if (in_array($value->getCountryId(), $this->valid_countries) && !$this->_alreadyInstalled($value->getCountryId())) {
+            if (in_array($value->getCountryId(), $this->valid_countries)) {
                 $options[$value->getCountryId()] = $value->getName();
             }
         }
@@ -39,18 +38,6 @@ class Dc_Regions_Model_Installer extends Varien_Object
         return $options;
     }
 
-    protected function _alreadyInstalled($countryCode){
-
-        if(is_null($this->_regions)){
-            $validCountries = $this->valid_countries;
-            $regions = Mage::getModel('directory/region')->getCollection()->addCountryFilter($validCountries);
-            $this->_regions = $regions;
-        }
-
-        $regionByCode = $this->_regions->getItemsByColumnValue('country_id',$countryCode);
-        return (bool)count($regionByCode)>0;
-
-    }
     /**
      * @param $country_id
      * @return bool
